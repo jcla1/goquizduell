b402a27e972f80f12b814e90c0dcd3ddbaa6fd3f
Joseph Adams <whitegolem@gmail.com>
2014-04-27 12:24:27 +0200

    go fmt

--- examples/avatar.go
+++ examples/avatar.go
@@ -1,11 +1,11 @@
 package main
 
 import (
-    "github.com/jcla1/goquizduell"
-    "github.com/jcla1/goquizduell/util"
+	"github.com/jcla1/goquizduell"
+	"github.com/jcla1/goquizduell/util"
 )
 
 func main() {
-    c := util.PrepareClient(os.Getenv("QD_USERNAME"), os.Getenv("QD_PASSWORD"), os.Getenv("QD_COOKIE_FILE"))
+	c := util.PrepareClient(os.Getenv("QD_USERNAME"), os.Getenv("QD_PASSWORD"), os.Getenv("QD_COOKIE_FILE"))
 
 }
