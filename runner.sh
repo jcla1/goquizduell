746accc27519353d5c73637df1afd886be270caa
Joseph Adams <whitegolem@gmail.com>
2014-05-02 18:08:32 +0200

    Updated runner.sh with new cmd line argument style.

--- runner.sh
+++ runner.sh
@@ -1,4 +1,4 @@
 while true; do
-    QD_COOKIE_FILE="cookie.gob" go run examples/main.go -constGames=115 -ansStdDev=0.7
+    QD_COOKIE_FILE="cookie.gob" go run examples/main.go -const-games=115 -ans-stddev=0.7
     sleep 60
 done
